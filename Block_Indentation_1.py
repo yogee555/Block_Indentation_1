6259abb9cab79896634a32323c2517a757a25785
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-06-29 14:58:38 +0530

    Add files via upload

---
+++ Block_Indentation_1.py
@@ -0,0 +1,5 @@
+# Block Indentation
+def my_function():             # This is a function defnation
+    a=2                        # This line belongs to the function because it's intended
+    return a                   # This line is also to the same function
+print(my_function())           # This function is out side the function block
